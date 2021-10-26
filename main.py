f60515939b9f3a76d12ee1cae49dcdb187c758d1
harakazu <info,harakazu@gmail.com>
2021-10-27 04:14:52 +0900

    googleでpythonを検索する。

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from selenium import webdriver
 from time import sleep
 
-# STEP1 : driverを作成
+# STEP1 : driverを作成。
 driver = webdriver.Chrome(
     executable_path="tools\chromedriver.exe")
 
-# STEP2 : driver.get()でサイトにアクセスする
+# STEP2 : driver.get()でサイトにアクセスする。
 driver.get("https://www.google.co.jp")
 sleep(3)
 
-# STEP3 : 要素を取得して、何らかの処理をかける
+# STEP3 : 要素を取得して、何らかの処理をかける。
 search_bar = driver.find_element_by_name("q")
 sleep(3)
 
